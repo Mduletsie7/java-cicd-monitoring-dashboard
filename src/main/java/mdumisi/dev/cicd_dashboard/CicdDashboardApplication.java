dff530102a5169c54802e3e557336885abb4de40
Mdumisi Kelvin Letsie <mdumisidev@gmail.com>
2025-07-12 23:47:35 +0200

    Initial repo set up

---
+++ src/main/java/mdumisi/dev/cicd_dashboard/CicdDashboardApplication.java
@@ -0,0 +1,13 @@
+package mdumisi.dev.cicd_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mdumisi/dev/cicd_dashboard/CicdDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package mdumisi.dev.cicd_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
